c6394e822863a27482cd49553b403b9ba5e924c1
ZhanKobe <1625589073@qq.com>
2020-06-15 21:17:00 +0800

    ##func004: 设置eureka注册集群

---
+++ cloud-eureka-server7002/src/main/java/com/james/springcloud/EurekaServer7002.java
@@ -0,0 +1,18 @@
+package com.james.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: JamesZhan
+ * @create: 2020 - 06 - 15 19:44
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002.class, args);
+    }
+}
