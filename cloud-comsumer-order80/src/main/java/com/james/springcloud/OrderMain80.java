499bee7d33e666eb0b065cb90057ffcc8e582e93
ZhanKobe <1625589073@qq.com>
2020-06-15 17:13:47 +0800

    ##func004: 添加Eureka作为注册中心

--- cloud-comsumer-order80/src/main/java/com/james/springcloud/OrderMain80.java
+++ cloud-comsumer-order80/src/main/java/com/james/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.james.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: JamesZhan
  * @create: 2020 - 06 - 14 23:06
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
